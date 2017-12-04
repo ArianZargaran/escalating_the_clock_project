07a3f68f6c7f20b8c34abfb42e1295b6a073eb64
ArianZargaran <arianzf@gmail.com>
2017-12-03 19:18:14 -0800

    problem solved

--- js/main.js
+++ js/main.js
@@ -1,67 +1,101 @@
-let container = document.getElementById("apple-watch-container")
-let newClock = '<h2 class="location">Pacific time</h2><div class="apple-watch"><div class="apple-watch__screen"><div class="clock analog_clock"><div class="dials"></div><div class="face"><div class="hands"><div class="hand hours-hand"></div><div class="hand minutes-hand"></div><div class="hand seconds-hand"></div></div><div class="numbers"><span class="number twelve">12</span><span class="number three">3</span><span class="number six">6</span><span class="number nine">9</span></div><div class="digital_clock"></div><div class="day_date"><span class="day"></span><span class="date"></span></div></div></div></div>'
-container.innerHTML = newClock
 
 let Watch = function Watch(gmt, timezone) {
   this.gmt = gmt;
   this.timezone = timezone;
 }
 
-Watch.prototype.getTime = function(){}
+Watch.prototype.getTime = function(){
+  let mainContainer = document.getElementById('apple-watch-container')
+  let newClock = document.createElement("DIV")
+  let thisClass = newClock.className = this.timezone.split(' ').join('') + "-watch" ;
+  newClock.innerHTML = '<h2 class="location">' + this.timezone + '</h2><div class="apple-watch"><div class="apple-watch__screen"><div class="clock analog_clock_' + thisClass +'"><div class="dials dials_' + thisClass +'"></div><div class="face"><div class="hands ' + thisClass + '-hands"><div class="hand hours-hand hours_' + thisClass + '"></div><div class="hand minutes-hand minutes_' + thisClass + '"></div><div class="hand seconds-hand seconds_' + thisClass + '"></div></div><div class="numbers"><span class="number twelve">12</span><span class="number three">3</span><span class="number six">6</span><span class="number nine">9</span></div><div class="digital_clock digital_' + thisClass + '"></div><div class="day_date day_date_'+ thisClass + '"><span class="day"></span><span class="date"></span></div></div></div></div>'
+  mainContainer.appendChild(newClock) 
 
-var madrid = new Watch(+1, "Madrid")
-
-
-
-//PAINTING THE WATCH//
-
-function dialCreation(){
-let getDials = document.querySelector(".dials")
+  let getDials = document.querySelector(".dials_" + thisClass)
+  
   for(var i = 0; i < 15; i++){
     let createDial = document.createElement('DIV')
-    createDial.className = 'dial dial_' + i;
+    createDial.className = 'dial dial_' + i + " " + thisClass;
     getDials.appendChild(createDial)
   }
+  
   let cloneDials = getDials.cloneNode(true)
   cloneDials.style.transform = "rotate(90deg)";
-  let printClone = document.querySelector(".analog_clock").appendChild(cloneDials)
+  let printClone = document.querySelector(".analog_clock_" + thisClass).appendChild(cloneDials)
+
+  let myGmt = this.gmt
+
+  var clock = function() {
+    //Get the time
+    let secondsHand = document.querySelector(".seconds_" + thisClass)
+    let minutesHand = document.querySelector(".minutes_" + thisClass)
+    let hoursHand = document.querySelector(".hours_" + thisClass)
+    let digitalClock = document.querySelector(".digital_" + thisClass)
+    let dateFeature = document.querySelector(".day_date_" + thisClass)
+    
+     const now = new Date()
+     let seconds = now.getSeconds();
+     let minutes = now.getMinutes();
+     let hours = now.getHours() + myGmt;
+
+     if(hours < 0) { hours += 12 }
+     else if(hours > 24) {hours -= 12 }
+
+     //Analog Clock
+     const secondsDegrees = (seconds / 60) * 360
+     const minutesDegrees = (minutes / 60) * 360
+     const hoursDegrees = (hours / 12 * 360) + minutes * .5
+    
+     secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+     minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+     hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    
+     //Digital Clock
+     digitalClock.innerHTML = round(hours) + ":" + round(minutes) + ":" + round(seconds)
+    
+     //Day & Date
+    dateFeature.children[0].innerHTML = getTheDay()
+    dateFeature.children[1].innerHTML = getTheDate()
+    
+    setInterval(clock, 1000)
+    }
+clock()
+
+
 }
-dialCreation();
 
 
 
-function clock(a) {
-//Get the time
-let secondsHand = document.querySelector(".seconds-hand")
-let minutesHand = document.querySelector(".minutes-hand")
-let hoursHand = document.querySelector(".hours-hand")
-let digitalClock = document.querySelector(".digital_clock")
-let dateFeature = document.querySelector(".day_date")
 
- const now = new Date()
- let seconds = now.getSeconds();
- let minutes = now.getMinutes();
- let hours = now.getHours() + a;
 
- //Analog Clock
- const secondsDegrees = (seconds / 60) * 360
- const minutesDegrees = (minutes / 60) * 360
- const hoursDegrees = (hours / 12 * 360) + minutes * .5
 
- secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
- minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
- hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+var santaClara = new Watch(-8, "Santa Clara")
+santaClara.getTime()
 
- //Digital Clock
- digitalClock.innerHTML = round(hours) + ":" + round(minutes) + ":" + round(seconds)
+var madrid = new Watch(1, "Madrid")
+madrid.getTime()
 
- //Day & Date
-dateFeature.children[0].innerHTML = getTheDay()
-dateFeature.children[1].innerHTML = getTheDate()
+var estambul = new Watch(3, "Estambul")
+estambul.getTime()
 
-setInterval(clock, 1000)
-}
-clock();
+var hawaii = new Watch(8, "Hawaii")
+hawaii.getTime()
+
+var berlin = new Watch(-10, "Berlín")
+berlin.getTime()
+
+var dublin = new Watch(0, "Dublín")
+dublin.getTime()
+
+var paris = new Watch(-5, "París")
+paris.getTime()
+
+var sanLorenzoDeCalatrava = new Watch(-8, "San Lorenzo")
+sanLorenzoDeCalatrava.getTime()
+
+
+
+
 
 
 function round(n){return n > 9 ? "" + n: "0" + n;}
@@ -75,11 +109,6 @@ function getTheDate(){return Date().substring(8,10);}
 
 // /////////
 // let nextId = 0;
-//
-// let Watch = function Watch(name, timezone) {
-//   let myThis = this;
-//   this.time = new Date();
-//   this.name = name;
 //
 //   this.myEl = $(`<div class='container-${nextId}'>`);
 //
